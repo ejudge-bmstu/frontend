63d90babff82cda88c7b819e2ff964718bd3f0de
JohnTer <36763228+JohnTer@users.noreply.github.com>
2019-03-22 23:37:12 +0300

    [EJ-40]Server v1.2 (#7)
    * Add simple server v1.1, ticket: EJ-38
    
    * add simple_server v1.2. Ticket: EJ-40
    
    * add simple_server v1.2. Ticket: EJ-40

--- simple_server/server.py
+++ simple_server/server.py
@@ -1,22 +1,27 @@
 import os
-from aiohttp import web
+from flask import Flask, send_file
 
 HOST = 'localhost'
 PORT = 8080
 
-INDEX_ROUTE = (os.path.abspath(os.path.dirname(__file__)) 
-               + "/static/index.html")
+SCRIPT_ROUTE = os.path.abspath(os.path.dirname(__file__))
+INDEX_FILE = SCRIPT_ROUTE + "/index.html"
+STATIC_ROUTE = "static/"
 
-routes = web.RouteTableDef()
+app = Flask(__name__)
 
-async def get_index(request):
-    return web.FileResponse(INDEX_ROUTE)
 
-def init():
-    app = web.Application()
-    app.router.add_route('GET', '/{tail:.*}', get_index)
-    return app
 
-if __name__ == "__main__":
-    web.run_app(init(), host=HOST, port = PORT)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_dir(path):
+	if STATIC_ROUTE == path[:len(STATIC_ROUTE)]:
+		return send_file(SCRIPT_ROUTE + "//" + path)
+	else:
+		return send_file(INDEX_FILE)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host=HOST, port=PORT)
 
